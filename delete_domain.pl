fa5998745670b4e079aa7e80d351741825708d2e
Victor Seva <vseva@sipwise.com>
2013-07-05 11:10:31 +0200

    delete_domain.pl: do not try to delete an nonexistent domain.

--- delete_domain.pl
+++ delete_domain.pl
@@ -15,12 +15,47 @@ die usage() unless ($#ARGV == 0);
 
 sub main {
     my ($bprov) = @_;
+
+    call_prov_exists( $bprov, 'get_domain',
+               {
+                    domain => $ARGV[0]
+               }
+             );
     call_prov( $bprov, 'delete_domain',
                {
                     domain => $ARGV[0]
                }
              );
     exit;
+}
+
+sub call_prov_exists {
+    #   scalar,    scalar,    hash-ref
+    my ($bprov, $function, $parameter) = @_;
+    my $result;
+
+    eval {
+        $result = $bprov->handle_request( $function,
+                                          {
+                                            authentication => {
+                                                                type     => 'admin',
+                                                                username => $CONFIG{admin},
+                                                                password => $CONFIG{password},
+                                                              },
+                                            parameters => $parameter,
+                                        });
+    };
+
+    if($@) {
+        if(ref $@ eq 'SOAP::Fault') {
+            exit 0 if($@->faultstring =~ 'unknown domain');
+            die "Billing\::$function failed: ". $@->faultstring;
+        } else {
+            die "Billing\::$function failed: $@";
+        }
+    }
+
+    return $result;
 }
 
 sub call_prov {
