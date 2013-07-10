721566900b2b3bc96b43b900dc4793327520e915
Victor Seva <vseva@sipwise.com>
2013-07-10 18:11:47 +0200

    use templates for tests

---
+++ generate_tests.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+BASE_DIR="/usr/local/src/kamailio-config-tests"
+LOG_DIR="${BASE_DIR}/log"
+RESULT_DIR="${BASE_DIR}/result"
+DOMAIN="spce.test"
+PROFILE="$1"
+TPAGE="/usr/bin/tpage"
+error_flag=0
+
+function usage
+{
+  echo "Usage: generate_tests.sh profile"
+  echo "profile: CE|PRO"
+}
+
+if [[ $# -ne 1 ]]; then
+  echo "Wrong number or arguments"
+  usage
+  exit 1
+fi
+
+if [ "${PROFILE}" == "CE" ]; then
+  TPAGE_ARGS=""
+elif [ "${PROFILE}" == "PRO" ]; then
+  TPAGE_ARGS="--define PRO=true"
+else
+  echo "PROFILE ${PROFILE} unknown"
+  usage
+  exit 2
+fi
+
+if [ ! -x ${TPAGE} ]; then
+  echo "Cannot exec ${TPAGE}"
+  usage
+  exit 3
+fi
+
+for t in $(find ${BASE_DIR}/scenarios/ -not -regex '.+customtt.tt2' -type f -name '*.tt2' | sort); do
+  template="$(basename $t)"
+  destdir="$(dirname $t)"
+  destfile="$(basename $t .tt2)"
+  custom_template="$(basename $t .tt2).customtt.tt2"
+
+  if [ -f "${destdir}/${custom_template}" ]; then
+    echo "Custom detected"
+    template=${custom_template}
+  fi
+  echo "generating: ${destdir}/${destfile}"
+  ${TPAGE} ${TPAGE_ARGS} ${destdir}/${template} > ${destdir}/${destfile}
+  if [ $? -ne 0 ]; then
+    error_flag=1
+  fi
+done
+
+exit $error_flag
+#EOF
--- run_tests.sh
+++ run_tests.sh
@@ -2,15 +2,38 @@
 BASE_DIR="/usr/local/src/kamailio-config-tests"
 LOG_DIR="${BASE_DIR}/log"
 RESULT_DIR="${BASE_DIR}/result"
+PROFILE="CE"
 DOMAIN="spce.test"
 error_flag=0
 
-while getopts 'ct' opt; do
+function usage
+{
+  echo "Usage: run_test.sh [-p PROFILE] [-c] [-t]"
+  echo "-p CE|PRO default is CE"
+  echo "-c skips configuration of the environment"
+  echo "-t skips tests"
+}
+
+while getopts 'ctp:' opt; do
   case $opt in
     c) SKIP=1;;
     t) TEST=1;;
+    p) PROFILE=$OPTARG;;
   esac
 done
+shift $(($OPTIND - 1))
+
+if [[ $# -ne 0 ]]; then
+  echo "Wrong number or arguments"
+  usage
+  exit 1
+fi
+
+if [ "${PROFILE}" != "CE" ] && [ "${PROFILE}" != "PRO" ]; then
+  echo "PROFILE ${PROFILE} unknown"
+  usage
+  exit 2
+fi
 
 if [ -z $SKIP ]; then
   ${BASE_DIR}/config_debug.pl on ${DOMAIN}
@@ -20,6 +43,12 @@ fi
 for i in ${LOG_DIR} ${RESULT_DIR}; do
   rm -rf $i
 done
+
+${BASE_DIR}/generate_tests.sh ${PROFILE}
+if [ $? -ne 0 ]; then
+  echo "Generating tests error"
+  exit 3
+fi
 
 for t in $(find ${BASE_DIR}/scenarios/ -depth -maxdepth 1 -mindepth 1 -type d | sort); do
   echo "Run: $(basename $t)"
