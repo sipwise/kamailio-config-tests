e1b117168b37899646f57b658d878db1b09b1782
Victor Seva <vseva@sipwise.com>
2013-07-12 15:31:52 +0200

    bin/sipp.sh: Added port, media port, timeout, help options

--- bin/sipp.sh
+++ bin/sipp.sh
@@ -17,20 +17,38 @@ function set_domain
   done
 }
 
-while getopts 'rd:' opt; do
+function usage
+{
+  echo "Usage: sipp.sh [-d domain] [-p PORT] [-m MPORT] [-t TIMEOUT] [-r] scenario.xml"
+  echo "Options:"
+  echo -e "\t-d: DOMAIN. default: spce.test"
+  echo -e "\t-p: sip port. default 50602/50603(responder)"
+  echo -e "\t-m: media port. default 6002/6003(responder)"
+  echo -e "\t-t: timeout. default 10/25(responder)"
+  echo "Arguments:"
+  echo -e "\t sipp_scenario.xml file"
+}
+
+while getopts 'hrp:m:t:d:' opt; do
   case $opt in
+    h) usage; exit 0;;
     r) RESP=1;;
-    d) DOMAIN=$OPTARG
+    p) PORT=$OPTARG;;
+    m) MPORT=$OPTARG;;
+    t) TIMEOUT=$OPTARG;;
+    d) DOMAIN=$OPTARG;;
   esac
 done
 shift $(($OPTIND - 1))
 
 if [[ $# -ne 1 ]]; then
-	echo "Usage: sipp.sh [-d domain] [-r] scenario.xml"
-	exit 1
+  echo "Wrong number of arguments"
+  usage
+  exit 1
 fi
 if [ ! -f $1 ]; then
 	echo "No $1 file found"
+	usage
 	exit 1
 fi
 BASE_DIR=$(dirname $1)
@@ -40,13 +58,17 @@ MAX="5000"
 set_domain 1
 
 if [ -z ${RESP} ]; then
-  PORT="50603"
+  MPORT=${PORT:-"6002"}
+  PORT=${PORT:-"50602"}
+  TIMEOUT=${TIMEOUT:-"10"}
   sipp -max_socket $MAX -inf ${BASE_DIR}/../callee.csv -inf ${BASE_DIR}/../caller.csv -sf $1 -i $IP \
-    -nd -t ul -p $PORT $IP -m 1 -mp 6003 -timeout 10 -timeout_error -trace_err
+    -nd -t ul -p $PORT $IP -m 1 -mp ${MPORT} -timeout ${TIMEOUT} -timeout_error -trace_err
 else
-  PORT="50602"
+  MPORT=${PORT:-"6003"}
+  PORT=${PORT:-"50603"}
+  TIMEOUT=${TIMEOUT:-"25"}
   sipp -max_socket $MAX -inf ${BASE_DIR}/../callee.csv -sf $1 -i $IP \
-    -nd -t ul -p $PORT $IP -m 1 -rtp_echo -mp 6002 -timeout 25 -timeout_error -trace_err
+    -nd -t ul -p $PORT $IP -m 1 -rtp_echo -mp ${MPORT} -timeout ${TIMEOUT} -timeout_error -trace_err
 fi
 
 set_domain 0
