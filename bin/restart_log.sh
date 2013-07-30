56b0e17c6708d7982f3859a79bdcb7f27940858a
Victor Seva <vseva@sipwise.com>
2013-07-30 09:21:00 +0200

    bin/restart_log.sh: clean kamailio-lb and sems logs. No restart is done.

--- bin/restart_log.sh
+++ bin/restart_log.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 invoke-rc.d kamailio-proxy stop
 rm -rf /var/log/ngcp/kamailio-proxy.log
+rm -rf /var/log/ngcp/sems.log
+rm -rf /var/log/ngcp/kamailio-lb.log
 invoke-rc.d rsyslog restart
 invoke-rc.d kamailio-proxy start
