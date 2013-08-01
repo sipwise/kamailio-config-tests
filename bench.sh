9f1e6c3f01d7d552e2a0ee26638aff82af3aef47
Victor Seva <vseva@sipwise.com>
2013-08-01 15:40:58 +0200

    bench.sh: helper script to run the process several times

---
+++ bench.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+NUM=${1:-20}
+for i in $(seq $NUM); do
+  BASE_DIR=$(pwd) ./run_tests.sh -c &> /tmp/run_tests.log
+  if [[ $? -ne 0 ]]; then
+  	echo "ERROR run_tests $i"
+  	break
+  fi
+  BASE_DIR=$(pwd) ./get_results.sh &> /tmp/get_results.log
+  if [[ $? -ne 0 ]]; then
+  	echo "ERROR get_results $i"
+  	break
+  fi
+  echo "$i done ok"
+done
