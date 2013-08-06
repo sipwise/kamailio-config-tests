aaef97d2fd80307f3267b83e97d280c7f0a29b4d
Victor Seva <vseva@sipwise.com>
2013-08-06 17:07:26 +0200

    bin/restart_log.sh: force kill kamailio and check if it starts

--- bin/restart_log.sh
+++ bin/restart_log.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 invoke-rc.d kamailio-proxy stop
+if [[ $(ps aux | grep kamailio.proxy | wc -l) -ne 1 ]]; then
+  echo "$(date) - killing kamailio-proxy"
+  for pid in $(ps aux | grep ^kamailio| grep kamailio.proxy | cut -d' ' -f2 | xargs); do
+    kill -9 $pid
+  done
+fi
 rm -rf /var/log/ngcp/kamailio-proxy.log
 rm -rf /var/log/ngcp/sems.log
 rm -rf /var/log/ngcp/kamailio-lb.log
 invoke-rc.d rsyslog restart
 invoke-rc.d kamailio-proxy start
+if [[ $(ps aux | grep kamailio.proxy | wc -l) -eq 1 ]]; then
+  exit 1
+fi
