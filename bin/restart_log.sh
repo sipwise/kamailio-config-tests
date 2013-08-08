cff0810739bf784d373728f99a970dc99b4e8833
Victor Seva <vseva@sipwise.com>
2013-08-08 17:10:40 +0200

    bin/restart_log.sh: Use the new kamailio RPC command to reset the msgid
    (cherry picked from commit e308ca7054793292c212c127e15ecaad13351d4f)

--- bin/restart_log.sh
+++ bin/restart_log.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-invoke-rc.d kamailio-proxy stop
-if [[ $(ps aux | grep kamailio.proxy | wc -l) -ne 1 ]]; then
-  echo "$(date) - killing kamailio-proxy"
-  for pid in $(ps aux | grep ^kamailio| grep kamailio.proxy | cut -d' ' -f2 | xargs); do
-    kill -9 $pid
-  done
-fi
+ngcp-sercmd proxy dbg.reset_msgid
 rm -rf /var/log/ngcp/kamailio-proxy.log
 rm -rf /var/log/ngcp/sems.log
 rm -rf /var/log/ngcp/kamailio-lb.log
 invoke-rc.d rsyslog restart
-invoke-rc.d kamailio-proxy start
-if [[ $(ps aux | grep kamailio.proxy | wc -l) -eq 1 ]]; then
-  exit 1
-fi
