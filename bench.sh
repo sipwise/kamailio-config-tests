489de491b265d31fa2762bcdcccf6c1ffd81e94a
Victor Seva <vseva@sipwise.com>
2013-08-08 10:54:10 +0200

    bench.sh: Added time info

--- bench.sh
+++ bench.sh
@@ -2,18 +2,19 @@
 NUM=${1:-20}
 # clean previous
 rm -rf log_* result_*
+echo "$(date) - Starting $NUM tests"
 for i in $(seq $NUM); do
   BASE_DIR=$(pwd) ./run_tests.sh -c &> /tmp/run_tests.log
   if [[ $? -ne 0 ]]; then
-  	echo "ERROR run_tests $i"
+  	echo "$(date) - ERROR run_tests $i"
   	break
   fi
   BASE_DIR=$(pwd) ./get_results.sh &> /tmp/get_results.log
   if [[ $? -ne 0 ]]; then
-  	echo "ERROR get_results $i"
+  	echo "$(date) - ERROR get_results $i"
   	break
   fi
-  echo "$i done ok"
+  echo "$(date) - $i done ok"
   # keep everything
   mv log log_$i 
   mv result result_$i
