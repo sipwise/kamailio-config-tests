3853ee2ce4d03792ca426de13edfc721ee365f2e
Victor Seva <vseva@sipwise.com>
2015-06-26 13:39:19 +0200

    MT#7571 [WIP] use NGCP REST API instead of SOAP lib
    missing API parts:
    - peers settings
    - add/modify/delete peers
      * bin/create_peers.pl
      * bin/update_peer_host.pl
    
    known problems:
    - boolean preferences
    
    TODO:
    - bin/create_ncos.pl
    - bin/create_rewrite_rules.pl
    - bin/create_subscribers_rewrite_rules.pl
    - bin/create_subscribers_speeddial_rules.pl
    
    Change-Id: I6d47e24669f9da22b5b3ac4fb410c50d2c9dbf53

--- bin/set_preferences.pl
+++ bin/set_preferences.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# Copyright: 2013 Sipwise Development Team <support@sipwise.com>
+# Copyright: 2013-2015 Sipwise Development Team <support@sipwise.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,130 +22,151 @@ use strict;
 use warnings;
 
 use English;
-use Getopt::Std;
-use Cwd 'abs_path';
 use YAML;
-use Sipwise::Provisioning::Voip;
-use Sipwise::Provisioning::Config;
+use Getopt::Long;
+use List::MoreUtils qw{ none };
+use Config::Tiny;
+use Sipwise::API qw(all);
+use Hash::Merge qw( merge );
 use Data::Dumper;
 
-our %CONFIG = ( admin    => 'cmd' );
+my $config =  Config::Tiny->read('/etc/default/ngcp-api');
+my $opts;
+if ($config) {
+	$opts = {};
+	$opts->{host} = $config->{_}->{NGCP_API_IP};
+	$opts->{port} = $config->{_}->{NGCP_API_PORT};
+	$opts->{sslverify} = $config->{_}->{NGCP_API_SSLVERIFY};
+}
+my $api = Sipwise::API->new($opts);
+$opts = $api->opts;
 
-my $config = Sipwise::Provisioning::Config->new()->get_config();
+sub usage {
+  return "Usage:\n$PROGRAM_NAME prefs.yml\n".
+		"Options:\n".
+		"  -d debug\n".
+		"  -h this help\n";
+}
+my $help = 0;
+GetOptions ("h|help" => \$help, "d|debug" => \$opts->{verbose})
+	or die("Error in command line arguments\n".usage());
 
-unless ($CONFIG{password} = $config->{acl}->{$CONFIG{admin}}->{password}) {
-  die "Error: No provisioning password found for user $CONFIG{admin}\n";
+die(usage()) unless (!$help);
+die("Wrong number of arguments\n".usage()) unless ($#ARGV == 0);
+
+sub set_subscriber_preferences
+{
+	my $subscriber = shift;
+	my $domain = shift;
+	my $prefs = shift;
+	my $subs_id = $api->check_subscriber_exists({
+							'domain'=>$domain,
+							'username'=>$subscriber});
+	if(defined $subs_id) {
+		my $subs_prefs = $api->get_subscriber_preferences($subs_id);
+		delete $subs_prefs->{_links};
+		my $subs_prefs_id = $subs_prefs->{id};
+		my $res = $api->set_subscriber_preferences($subs_prefs_id,
+					merge($prefs, $subs_prefs));
+		if($opts->{verbose}) {
+			print Dumper $res;
+		}
+		if(defined $res) {
+			print "prefs created for ${subscriber}\@${domain}\n";
+		}
+	}
+	else {
+		die("No subscriber ${subscriber}\@${domain} found");
+	}
+	return;
 }
 
-sub main;
-sub usage;
-sub call_prov;
-
-die usage() unless ($#ARGV == 0);
-
-our $vprov = Sipwise::Provisioning::Voip->new();
-
-main();
-
-sub get_peers
+sub set_domain_preferences
 {
-    my $peers;
-    my $result = call_prov( $vprov, 'get_peer_groups');
-    foreach (@{$result})
-    {
-        my $res = call_prov( $vprov, 'get_peer_group_details', {id=>$_->{id}});
-        foreach (@{$res->{peers}})
-        {
-            $peers->{$_->{name}} = $_->{id};
-        }
-    }
-    return $peers;
+	my $domain = shift;
+	my $prefs = shift;
+	my $domain_id = $api->check_domain_exists({'domain' => $domain});
+
+	if(defined $domain_id) {
+		my $dom_prefs = $api->get_domain_preferences($domain_id);
+		my $links = delete $dom_prefs->{_links};
+		my $dom_prefs_id = $dom_prefs->{id};
+		my $res = $api->set_domain_preferences($dom_prefs_id,
+					merge($prefs, $dom_prefs));
+		if($opts->{verbose}) {
+			print Dumper $res;
+		}
+		if(defined $res) {
+			print "prefs created for ${domain}\n";
+		}
+	}
+	else {
+		die("No domain ${domain} found");
+	}
+	return;
 }
 
-sub get_rewrites
+sub set_peer_preferences
 {
-    my $rule_set;
-    my $result = call_prov( $vprov, 'get_rewrite_rule_sets');
-    foreach (@{$result})
-    {
-        $rule_set->{$_->{name}} = $_->{id};
-    }
-    return $rule_set;
+	my $id = shift;
+	my $prefs = shift;
+	my $peer_id = $api->check_peer_exists($id);
+
+	if(defined $peer_id) {
+		my $peer_prefs = $api->get_peer_preferences($peer_id);
+		delete $peer_prefs->{_links};
+		my $peer_prefs_id = $peer_prefs->{id};
+		return $api->set_peer_preferences($peer_prefs_id,
+					merge($prefs, $peer_prefs));
+	}
+	else {
+		die("No peer ${id} found");
+	}
+	return;
 }
 
 sub main {
-    my $filename = abs_path($ARGV[0]);
-    my $prefs = YAML::LoadFile($filename);
-    my $peers;
-    my $rule_set;
+	my $prefs = shift;
+	my $peers;
+	my $rule_set;
 
-    for my $key (keys $prefs)
-    {
-        if (exists($prefs->{$key}->{rewrite_rule_set}))
-        {
-            if(!$rule_set) { $rule_set = get_rewrites($vprov); }
-            if (exists($rule_set->{$prefs->{$key}->{rewrite_rule_set}}))
-            {
-                $prefs->{$key}->{rewrite_rule_set} = $rule_set->{$prefs->{$key}->{rewrite_rule_set}};
-            }
-            else
-            {
-                die("No rewrite_rule_set:$prefs->{$key}->{rewrite_rule_set} found");
-            }
-        }
-        if ( $key =~ /@/ )
-        {
-            my @fields = split /@/, $key;
-            if (!$fields[0])
-            {
-                my $pref = { domain => $fields[1], preferences => $prefs->{$key} };
-                call_prov( $vprov, 'set_domain_preferences',  $pref);
-            }
-            else
-            {
-                my $pref = { username => $fields[0], domain => $fields[1], preferences => $prefs->{$key} };
-                call_prov( $vprov, 'set_subscriber_preferences',  $pref);
-            }
-        }
-        else
-        {
-            if (!$peers) { $peers = get_peers($vprov); }
-            my $pref = { id => $peers->{$key}, preferences => $prefs->{$key} };
-            call_prov( $vprov, 'set_peer_preferences',  $pref);
-        }
-    }
+	for my $key (keys %{$prefs})
+	{
+		if (exists($prefs->{$key}->{rewrite_rule_set}))
+		{
+			my $rule_set_id = $api->check_rewriterule_exists(
+								$prefs->{$key}->{rewrite_rule_set});
+			if (defined $rule_set_id)
+			{
+				$prefs->{$key}->{rewrite_rule_set} = $rule_set->{$prefs->{$key}->{rewrite_rule_set}};
+			}
+			else
+			{
+				die("No rewrite_rule_set:$prefs->{$key}->{rewrite_rule_set} found");
+			}
+		}
+		if ( $key =~ /@/ )
+		{
+			my @fields = split /@/, $key;
+			if (!$fields[0])
+			{
+				set_domain_preferences($fields[1], $prefs->{$key});
+			}
+			else
+			{
+				set_subscriber_preferences($fields[0], $fields[1], $prefs->{$key});
+			}
+		}
+		else
+		{
+			#set_peer_preferences($key, $prefs->{$key});
+			die "API peer preferences *Not* implemented";
+		}
+	}
 
-    exit;
+	exit;
 }
 
-sub call_prov {
-    #   scalar,    scalar,    hash-ref
-    my ($prov, $function, $parameter) = @_;
-    my $result;
+my $cf = YAML::LoadFile($ARGV[0]);
 
-    eval {
-        $result = $prov->handle_request( $function,
-                                          {
-                                            authentication => {
-                                                                type     => 'system',
-                                                                username => $CONFIG{admin},
-                                                                password => $CONFIG{password},
-                                                              },
-                                            parameters => $parameter,
-                                        });
-    };
-
-    if($EVAL_ERROR) {
-        if(ref $EVAL_ERROR eq 'SOAP::Fault') {
-            die "Billing\::$function failed: ". $EVAL_ERROR->faultstring;
-        } else {
-            die "Billing\::$function failed: $EVAL_ERROR";
-        }
-    }
-
-    return $result;
-}
-
-sub usage {
-    die "Usage:\n$PROGRAM_NAME prefs.yml\n";
-}
+main($cf);
