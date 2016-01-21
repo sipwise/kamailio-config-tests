4c4dbe03d786d3dfda4b4fe2133c3e3c36b1e89a
Victor Seva <vseva@sipwise.com>
2016-01-21 22:28:27 +0100

    MT#7571 provisioning via REST API step two
    * migrate provisioning:
      - ncos
      - speeddial
      - callforward
      - peering
    * enable peer scenarios
    * add 'lock' property support for subscriber
    
    Change-Id: I15e9afb744f6f4700dd863bfe226944ea4c3ecf7

--- bin/set_subscribers_callforward.pl
+++ bin/set_subscribers_callforward.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 #
-# Copyright: 2013 Sipwise Development Team <support@sipwise.com>
+# Copyright: 2013-2016 Sipwise Development Team <support@sipwise.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,115 +22,102 @@ use strict;
 use warnings;
 
 use English;
-use Getopt::Std;
-use Cwd 'abs_path';
 use YAML;
-use Sipwise::Provisioning::Voip;
-use Sipwise::Provisioning::Config;
+use Getopt::Long;
+use Cwd 'abs_path';
+use Config::Tiny;
+use Sipwise::API qw(all);
+use Data::Dumper;
 
-our %CONFIG = ( admin    => 'cmd' );
-
-my $config = Sipwise::Provisioning::Config->new()->get_config();
-
-unless ($CONFIG{password} = $config->{acl}->{$CONFIG{admin}}->{password}) {
-  die "Error: No provisioning password found for user $CONFIG{admin}\n";
+my $config =  Config::Tiny->read('/etc/default/ngcp-api');
+my $opts;
+if ($config) {
+  $opts = {};
+  $opts->{host} = $config->{_}->{NGCP_API_IP};
+  $opts->{port} = $config->{_}->{NGCP_API_PORT};
+  $opts->{sslverify} = $config->{_}->{NGCP_API_SSLVERIFY};
 }
-
-sub main;
-sub usage;
-sub call_prov;
-
-die usage() unless ($#ARGV == 0);
-
-my $bprov = Sipwise::Provisioning::Voip->new();
-
-main;
-
-sub main {
-    my $filename = abs_path($ARGV[0]);
-    my $cf = YAML::LoadFile($filename);
-
-    for my $key (keys $cf)
-    {
-        my @fields = split /@/, $key;
-        my $param;
-        # cf_set
-        my $set = {}; # name -> set_id
-        my $time = {}; # name -> time_id
-        foreach my $s (@{$cf->{$key}->{'destination_set'}})
-        {
-          $param = { username => $fields[0], domain => $fields[1], data => $s };
-          call_prov( 'create_subscriber_cf_destination_set',  $param);
-        }
-        $param = { username => $fields[0], domain => $fields[1] };
-        my $result = call_prov('get_subscriber_cf_destination_sets', $param);
-        # name -> set_id
-        foreach my $r (@{$result})
-        {
-          $set->{$r->{'name'}} = $r->{'id'};
-        }
-        # time_set
-        foreach my $t (@{$cf->{$key}->{'time_set'}})
-        {
-          $param = { username => $fields[0], domain => $fields[1], data => $t };
-          call_prov( 'create_subscriber_cf_time_set',  $param);
-        }
-        $param = { username => $fields[0], domain => $fields[1] };
-        $result = call_prov('get_subscriber_cf_time_sets', $param);
-        # name -> time_id
-        foreach my $r (@{$result})
-        {
-          $time->{$r->{'name'}} = $r->{'id'};
-        }
-        # cf_map
-        foreach my $m (@{$cf->{$key}->{'map'}})
-        {
-          $m->{'destination_set_id'} = $set->{$m->{'destination_set_id'}};
-          if(exists($m->{'time_set_id'}))
-          {
-            $m->{'time_set_id'} = $time->{$m->{'time_set_id'}};
-          }
-          $param = { username => $fields[0], domain => $fields[1], data => $m };
-          call_prov( 'create_subscriber_cf_map',  $param);
-        }
-        # voicebox
-        if (exists($cf->{$key}->{voicebox}))
-        {
-          $param = { username => $fields[0], domain => $fields[1], preferences => $cf->{$key}->{voicebox}};
-          call_prov( 'set_subscriber_voicebox_preferences',  $param);
-        }
-    }
-    exit;
-}
-
-sub call_prov {
-    #   scalar,    scalar,    hash-ref
-    my ($function, $parameter) = @_;
-    my $result;
-
-    eval {
-        $result = $bprov->handle_request( $function,
-                                          {
-                                            authentication => {
-                                                                type     => 'system',
-                                                                username => $CONFIG{admin},
-                                                                password => $CONFIG{password},
-                                                              },
-                                            parameters => $parameter,
-                                        });
-    };
-
-    if($EVAL_ERROR) {
-        if(ref $EVAL_ERROR eq 'SOAP::Fault') {
-            die "Voip\::$function failed: ". $EVAL_ERROR->faultstring;
-        } else {
-            die "Voip\::$function failed: $EVAL_ERROR";
-        }
-    }
-
-    return $result;
-}
+my $api = Sipwise::API->new($opts);
+$opts = $api->opts;
 
 sub usage {
-    die "Usage:\n$PROGRAM_NAME callforward.yml\n";
+  return  "Usage:\n$PROGRAM_NAME callforward.yml\n".
+          "Options:\n".
+          "  -d debug\n".
+          "  -h this help\n";
 }
+my $help = 0;
+GetOptions(
+  "h|help" => \$help,
+  "d|debug" => \$opts->{verbose}
+) or die("Error in command line arguments\n".usage());
+
+die(usage()) unless (!$help);
+die("Wrong number of arguments\n".usage()) unless ($#ARGV == 0);
+
+sub set_subscriber_callforward
+{
+  my $subscriber = shift;
+  my $domain = shift;
+  my $prefs = shift;
+  my $subs_id = $api->check_subscriber_exists({
+                          domain => $domain,
+                          username => $subscriber});
+  if($subs_id) {
+    my $res = $api->set_subscriber_callforward($subs_id, $prefs);
+    if($opts->{verbose}) {
+      print Dumper $res;
+    }
+    if($res) {
+      print "callforward created for ${subscriber}\@${domain} [$subs_id]\n";
+    } else {
+      die("callforward failed for ${subscriber}\@${domain} [$subs_id]");
+    }
+  }
+  else {
+    die("No subscriber ${subscriber}\@${domain} found");
+  }
+  return;
+}
+
+sub set_subscriber_voicemailsetting
+{
+  my $subscriber = shift;
+  my $domain = shift;
+  my $prefs = shift;
+  my $subs_id = $api->check_subscriber_exists({
+                          domain => $domain,
+                          username => $subscriber});
+  if($subs_id) {
+    my $res = $api->set_subscriber_voicemailsetting($subs_id, $prefs);
+    if($opts->{verbose}) {
+      print Dumper $res;
+    }
+    if($res) {
+      print "voicemailsetting created for ${subscriber}\@${domain} [$subs_id]\n";
+    } else {
+      die("voicemailsetting failed for ${subscriber}\@${domain} [$subs_id]");
+    }
+  }
+  else {
+    die("No subscriber ${subscriber}\@${domain} found");
+  }
+  return;
+}
+
+sub main {
+    my $r = YAML::LoadFile(abs_path($ARGV[0]));
+
+    for my $key (keys %{$r})
+    {
+        my @fields = split /@/, $key;
+        my $voicemail = delete $r->{$key}->{voicebox};
+        set_subscriber_callforward($fields[0], $fields[1], $r->{$key});
+        if ($voicemail) {
+          set_subscriber_voicemailsetting($fields[0], $fields[1], $voicemail);
+        }
+    }
+    return;
+}
+
+main();
