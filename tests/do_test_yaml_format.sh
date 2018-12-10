963fa80b507a9216771656c03494285d2e624995
Marco Capetta <mcapetta@sipwise.com>
2018-12-10 17:14:04 +0100

    TT#48618 Backport test improvements to old versions
    In particular:
    * Test timing configurations
    * JSON folder creation and migration
    * Retransmissions check during test execution
    * Retransmissions check during test validation
    * Single sip trace containing all the scenarios
    * CDR validation (test not populated)
    
    (partially cherry picked from commit 06faebb436ca1be9ef2fe94c37740f479280fd48)
    
    Change-Id: I06c8df802dbeab3f63a18b6f273491143518feec

--- tests/do_test_yaml_format.sh
+++ tests/do_test_yaml_format.sh
@@ -2,7 +2,7 @@
 export BASE_DIR=$(pwd) DEST_DIR=${RESULTS}
 
 ./bin/generate_tests.sh -d "$1" tests/fixtures/scenario_ids.yml PRO
-find "${RESULTS}/$1" -type f -name '*_test.yml' | while read -r i; do
+find "${RESULTS}/$1" -type f -name '[0-9][0-9][0-9][0-9]_test.yml' | while read -r i; do
   ./tests/test_yaml_format.py "$i" | sed -e 's/skip>/skipped>/g' > "$i.xml"
 done
 
