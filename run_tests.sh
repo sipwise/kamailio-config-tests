9e02279da6a231418333a75bd2c889a75b28cbfb
Marco Capetta <mcapetta@sipwise.com>
2018-12-11 17:02:29 +0100

    TT#48618 Backport test improvements to old versions
    In particular:
    * Test timing configurations
    * JSON folder creation and migration
    * Retransmissions check during test execution
    * Retransmissions check during test validation
    * Single sip trace containing all the scenarios
    * CDR validation (test not populated)
    
    (partially cherry picked from commit 06faebb436ca1be9ef2fe94c37740f479280fd48)
    
    Change-Id: If6d6b84a17a02781c0c4c9857c7b47d31cfa9816

--- run_tests.sh
+++ run_tests.sh
@@ -7,39 +7,32 @@ BIN_DIR="${BASE_DIR}/bin"
 GROUP="${GROUP:-scenarios}"
 LOG_DIR="${BASE_DIR}/log/${GROUP}"
 MLOG_DIR="${BASE_DIR}/mem"
+KAM_DIR="/tmp/cfgtest"
 PROFILE="CE"
+OPTS=(-P -T)
 DOMAIN="spce.test"
 TIMEOUT=${TIMEOUT:-300}
+SHOW_SCENARIOS=false
+SKIP_CONFIG=false
+CAPTURE=false
+FIX_RETRANS=false
+MEMDBG=false
+CDR=false
+START_TIME=$(date +%s)
 error_flag=0
 
-function usage
-{
-  echo "Usage: run_test.sh [-p PROFILE] [-c] [-t]"
-  echo "-p CE|PRO default is CE"
-  echo "-l print available SCENARIOS in GROUP"
-  echo "-c skips configuration of the environment"
-  echo "-K capture messages with tcpdump"
-  echo "-x set GROUP scenario. Default: scenarios"
-  echo "-t set timeout in secs for pid_watcher.py [PRO]. Default: 300"
-  echo "-h this help"
-
-  echo "BASE_DIR:${BASE_DIR}"
-  echo "BIN_DIR:${BIN_DIR}"
-}
-
-function get_scenarios
-{
+get_scenarios() {
   local t
   local flag
   flag=0
   if [ -n "${SCENARIOS}" ]; then
     for t in ${SCENARIOS}; do
-      if [ ! -d "${BASE_DIR}/${GROUP}/$t" ]; then
+      if [ ! -d "${BASE_DIR}/${GROUP}/${t}" ]; then
         echo "$(date) - scenario: $t at ${GROUP} not found"
         flag=1
       fi
     done
-    if [ $flag != 0 ]; then
+    if [ ${flag} != 0 ]; then
       exit 1
     fi
   else
@@ -48,28 +41,160 @@ function get_scenarios
   fi
 }
 
-function cfg_debug_off
-{
-  if [ -z "$SKIP" ]; then
+cfg_debug_off() {
+  if ! "${SKIP_CONFIG}" ; then
+    echo "$(date) - Removed apicert.pem"
+    rm -f "${BASE_DIR}/apicert.pem"
     echo "$(date) - Setting config debug off"
     "${BIN_DIR}/config_debug.pl" -g "${GROUP}" off ${DOMAIN}
     if ! ngcpcfg apply "config debug off via kamailio-config-tests" ; then
       echo "$(date) - ngcpcfg apply returned $?"
       error_flag=4
     fi
-    echo "$(date) - Setting config debug off. Done[$error_flag]"
+    echo "$(date) - Setting config debug off. Done[${error_flag}]"
   fi
 }
 
-while getopts 'hlcp:Kx:t:' opt; do
+capture() {
+  echo "$(date) - ================================================================================="
+  echo "$(date) - Start tcpdump captures"
+  datetime=$(date '+%y%m%d_%H%M')
+  for inter in $(ip link | grep '^[0-9]' | cut -d: -f2 | sed 's/ //' | xargs); do
+    tcpdump -i "${inter}" -n -s 65535 -w "${LOG_DIR}/_traces_${inter}_${datetime}.pcap" &
+    capture_pid="${capture_pid} ${inter}:$!"
+  done
+  echo "$(date) - Done"
+  echo "$(date) - ================================================================================="
+}
+
+stop_capture() {
+  echo "$(date) - ================================================================================="
+  echo "$(date) - Stop tcpdump captures"
+  local inter=""
+  local temp_pid=""
+  if [ -n "${capture_pid}" ]; then
+    for temp in ${capture_pid}; do
+      inter=$(echo "$temp"|cut -d: -f1)
+      temp_pid=$(echo "$temp"|cut -d: -f2)
+      #echo "inter:${inter} temp_pid:${temp_pid}"
+      if ps -p"${temp_pid}" &> /dev/null ; then
+        echo "$(date) - End ${inter}[${temp_pid}] capture"
+        kill -15 "${temp_pid}"
+      fi
+    done
+  fi
+  echo "$(date) - Done"
+  echo "$(date) - ================================================================================="
+}
+
+move_json_file() {
+  echo "$(date) - ================================================================================="
+  echo "$(date) - Move kamailio json files"
+  for t in ${SCENARIOS}; do
+    echo "$(date) - - Scenario: ${t}"
+    json_dir="${KAM_DIR}/${t}"
+    if [ -d "${json_dir}" ] ; then
+      for i in "${json_dir}"/*.json ; do
+        json_size_before=$(stat -c%s "${i}")
+        moved_file="${LOG_DIR}/${t}/$(printf "%04d.json" "$(basename "$i" .json)")"
+        expand -t1 "${i}" > "${moved_file}"
+        json_size_after=$(stat -c%s "${moved_file}")
+        echo "$(date) - - - Moved file ${i} with size before: ${json_size_before} and after: ${json_size_after}"
+        rm "${i}"
+      done
+      rm -rf "${json_dir}"
+    fi
+  done
+
+  echo "$(date) - Done"
+  echo "$(date) - ================================================================================="
+}
+
+fix_retransmissions() {
+  echo "$(date) - ================================================================================="
+  echo "$(date) - Checking retransmission issues"
+  for t in ${SCENARIOS}; do
+    echo "$(date) - - Scenario: ${t}"
+    RETRANS_ISSUE=false
+    file_find=($(find "${LOG_DIR}/${t}" -maxdepth 1 -name '*.json' | sort))
+    for json_file in "${file_find[@]}" ; do
+      file_find=("${file_find[@]:1}")
+      if ! [ -a "${json_file}" ] ; then
+        continue
+      fi
+      for next_json_file in "${file_find[@]}" ; do
+        if ! [ -a "${next_json_file}" ] ; then
+          continue
+        fi
+        if ( diff -q -u <(tail -n3 "${json_file}") <(tail -n3 "${next_json_file}") &> /dev/null ) ; then
+          echo "$(date) - - - $(basename "${next_json_file}") seems a retransmission of $(basename "${json_file}") ---> renaming the file in $(basename "${next_json_file}")_retransmission"
+          mv -f "${next_json_file}" "${next_json_file}_retransmission"
+          RETRANS_ISSUE=true
+        fi
+      done
+    done
+
+    if "${RETRANS_ISSUE}" ; then
+      echo "$(date) - - - Reordering kamailio json files"
+      file_find=($(find "${LOG_DIR}/${t}" -maxdepth 1 -name '*.json' | sort))
+      a=1
+      for json_file in "${file_find[@]}" ; do
+        new_name=$(printf "%04d.json" "${a}")
+        mv -n "${json_file}" "${LOG_DIR}/${t}/${new_name}" &> /dev/null
+        let a=a+1
+      done
+    fi
+  done
+
+  echo "$(date) - Done"
+  echo "$(date) - ================================================================================="
+}
+
+cdr_export() {
+  echo "$(date) - ================================================================================="
+  echo "$(date) - Extracting CDRs"
+  for t in ${SCENARIOS}; do
+    echo "$(date) - - Scenario: $t"
+    if ! "${BIN_DIR}/cdr_extract.sh" -m -t "${START_TIME}" -s "${GROUP}" "${t}" ; then
+      echo "ERROR: ${t}"
+      error_flag=1
+    fi
+  done
+
+  echo "$(date) - Done"
+  echo "$(date) - ================================================================================="
+}
+
+usage() {
+  echo "Usage: run_test.sh [-p PROFILE] [-C] [-t]"
+  echo "Options:"
+  echo -e "\t-p CE|PRO default is CE"
+  echo -e "\t-l print available SCENARIOS in GROUP"
+  echo -e "\t-C skips configuration of the environment"
+  echo -e "\t-K capture messages with tcpdump"
+  echo -e "\t-x set GROUP scenario. Default: scenarios"
+  echo -e "\t-t set timeout in secs for pid_watcher.py [PRO]. Default: 300"
+  echo -e "\t-r fix retransmission issues"
+  echo -e "\t-c export CDRs at the end of the test"
+  echo -e "\t-m mem debug"
+  echo -e "\t-h this help"
+
+  echo "BASE_DIR:${BASE_DIR}"
+  echo "BIN_DIR:${BIN_DIR}"
+}
+
+while getopts 'hlCcp:Kx:t:rm' opt; do
   case $opt in
     h) usage; exit 0;;
-    l) SHOW_SCENARIOS=1;;
-    c) SKIP=1;;
-    p) PROFILE=$OPTARG;;
-    K) SKIP_CAPTURE=1;;
-    x) GROUP=$OPTARG;;
-    t) TIMEOUT=$OPTARG;;
+    l) SHOW_SCENARIOS=true;;
+    C) SKIP_CONFIG=true;;
+    p) PROFILE=${OPTARG};;
+    K) CAPTURE=true;;
+    x) GROUP=${OPTARG};;
+    t) TIMEOUT=${OPTARG};;
+    r) FIX_RETRANS=true;;
+    c) CDR=true;;
+    m) MEMDBG=true;;
   esac
 done
 shift $((OPTIND - 1))
@@ -80,7 +205,7 @@ if [[ $# -ne 0 ]]; then
   exit 1
 fi
 
-if [[ ${SHOW_SCENARIOS} = 1 ]] ; then
+if "${SHOW_SCENARIOS}"  ; then
   get_scenarios
   echo "${SCENARIOS}"
   exit 0
@@ -92,7 +217,7 @@ if [ "${PROFILE}" != "CE" ] && [ "${PROFILE}" != "PRO" ]; then
   exit 2
 fi
 
-if [ "$GROUP" = "scenarios_pbx" ] ; then
+if [ "${GROUP}" = "scenarios_pbx" ] ; then
   PIDWATCH_OPTS="--pbx"
   # hack for pid_watcher ( sems-pbx was not active )
   mkdir -p /var/run/sems-pbx/
@@ -102,15 +227,24 @@ else
   PIDWATCH_OPTS=""
 fi
 
+LOG_DIR="${BASE_DIR}/log/${GROUP}"
+
+echo "$(date) - Create temporary folder for json files"
+rm -rf "${KAM_DIR}"
+mkdir -p "${KAM_DIR}"
+if [ -d "${KAM_DIR}" ]; then
+  chown kamailio:kamailio "${KAM_DIR}"
+fi
+
 echo "$(date) - Clean mem log dir"
 rm -rf "${MLOG_DIR}"
 mkdir -p "${MLOG_DIR}" "${LOG_DIR}"
 
-if [ -z $SKIP ]; then
+if ! "${SKIP_CONFIG}" ; then
   echo "$(date) - Setting config debug on"
   "${BIN_DIR}/config_debug.pl" -g "${GROUP}" on ${DOMAIN}
   if [ "${PROFILE}" == "PRO" ]; then
-    echo "$(date) - Exec pid_watcher with timeout[$TIMEOUT]"
+    echo "$(date) - Exec pid_watcher with timeout[${TIMEOUT}]"
     ( timeout "${TIMEOUT}" "${BIN_DIR}/pid_watcher.py" ${PIDWATCH_OPTS} )&
   fi
   if ! ngcpcfg apply "config debug on via kamailio-config-tests" ; then
@@ -129,40 +263,87 @@ if [ -z $SKIP ]; then
       exit 1
     fi
   fi
-  echo "$(date) - Setting config debug on. Done[$error_flag]."
+  echo "$(date) - Setting config debug on. Done[${error_flag}]."
 fi
 
 echo "$(date) - Initial mem stats"
 VERSION="${PROFILE}_$(cut -f1 -d' '< /etc/ngcp_version)_"
 "${BIN_DIR}/mem_stats.py" --private_file="${MLOG_DIR}/${VERSION}_${GROUP}_initial_pvm.cvs" \
   --share_file="${MLOG_DIR}/${VERSION}_${GROUP}_initial_shm.cvs"
+if "${MEMDBG}" ; then
+  ngcp-memdbg-csv /var/log/ngcp/kamailio-proxy.log "${MLOG_DIR}" >/dev/null
+fi
 
 get_scenarios
 
-if [[ ${SKIP_CAPTURE} = 1 ]] ; then
-  echo "$(date) enable capture"
-  OPTS+="-K"
+if "${MEMDBG}" ; then
+  echo "$(date) - enable memdbg"
+  OPTS+=(-m)
+fi
+
+if "${CDR}" ; then
+  echo "$(date) - enable cdr export at the end of the execution"
+fi
+
+if "${CAPTURE}" ; then
+  capture
 fi
 
 for t in ${SCENARIOS}; do
-  echo "$(date) - Run[${GROUP}/${PROFILE}]: $t ================================================="
+  echo "$(date) - ================================================================================="
+  echo "$(date) - Run [${GROUP}/${PROFILE}]: ${t}"
+
   log_temp="${LOG_DIR}/${t}"
   if [ -d "${log_temp}" ]; then
     echo "$(date) - Clean log dir"
     rm -rf "${log_temp}"
   fi
-  if ! "${BIN_DIR}/check.sh" ${OPTS} -J -P -T -d ${DOMAIN} -p "${PROFILE}" -s "${GROUP}" "$t" ; then
-    echo "ERROR: $t"
+
+  json_temp="${KAM_DIR}/${t}"
+  if [ -d "${json_temp}" ]; then
+    echo "$(date) - Clean json dir"
+    rm -rf "${json_temp}"
+  fi
+
+  if ! "${BIN_DIR}/check.sh" "${OPTS[@]}" -d "${DOMAIN}" -p "${PROFILE}" -s "${GROUP}" "${t}" ; then
+    echo "ERROR: ${t}"
     error_flag=1
   fi
+
   echo "$(date) - ================================================================================="
 done
+
+# Hack to allow tcpdump to capture all the packages and kamailio to write all the json files
+sleep 5
+
+if "${CAPTURE}" ; then
+  stop_capture
+fi
+
+move_json_file
+
+if "${FIX_RETRANS}" ; then
+  fix_retransmissions
+fi
+
+if "${CDR}" ; then
+  sleep 2
+  cdr_export
+fi
 
 echo "$(date) - Final mem stats"
 "${BIN_DIR}/mem_stats.py" --private_file="${MLOG_DIR}/${VERSION}_${GROUP}_final_pvm.cvs" \
   --share_file="${MLOG_DIR}/${VERSION}_${GROUP}_final_shm.cvs"
+if [[ ${MEMDBG} = 1 ]] ; then
+  ngcp-memdbg-csv /var/log/ngcp/kamailio-proxy.log "${MLOG_DIR}" >/dev/null
+fi
+
+if [ -d "${KAM_DIR}" ]; then
+  echo "$(date) - Removing temporary json dir"
+  rm -rf "${KAM_DIR}"
+fi
 
 cfg_debug_off
 
-echo "$(date) - Done[$error_flag]"
-exit $error_flag
+echo "$(date) - Done[${error_flag}]"
+exit ${error_flag}
