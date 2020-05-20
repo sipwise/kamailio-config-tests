64cd8cb0797ed5432fc740531def9c7227e6f441
Victor Seva <vseva@sipwise.com>
2020-05-20 11:45:59 +0200

    TT#81700 generate_test_tt2.pl: add P-LB-Uptime to common subst headers
    * remove invite_conference_update media file, this has to be a link
    * ngcpcfg apply executed outside /etc/ngcp-config seems to generate
      .ngcpcfg_perms file in $pwd so adding a workaround to avoid it
    * bench.sh: support selecting the GROUP scenario
    * show_flow_diff.pl: improve the diff output adding the values of
      the flows not only the names
    
    Change-Id: I40db8433595168ce8d3ef061f77f02a1616ed980
    (cherry picked from commit 36349d44d94474719fa5ecd63f924f2b5a8ea4b1)

--- bench.sh
+++ bench.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 SKIP_CONFIG=false
 PROFILE=CE
+GROUP="${GROUP:-scenarios}"
 
 usage() {
   echo "Usage: bench.sh [-p PROFILE] [-C] [num_runs]"
   echo "Options:"
   echo -e "\t-p CE|PRO default is CE"
   echo -e "\t-C skips configuration of the environment"
+  echo -e "\t-x set GROUP scenario. Default: scenarios"
   echo -e "\t-h this help"
   echo -e "num_runs default is 20"
 }
 
-while getopts 'hCp:' opt; do
+while getopts 'hCp:x:' opt; do
   case $opt in
     h) usage; exit 0;;
     C) SKIP_CONFIG=true;;
     p) PROFILE=${OPTARG};;
+    x) GROUP=${OPTARG};;
 	*) echo "Unknown option $opt"; usage; exit 1;;
   esac
 done
@@ -28,7 +31,7 @@ if [ "${PROFILE}" != "CE" ] && [ "${PROFILE}" != "PRO" ]; then
 fi
 
 NUM=${1:-20}
-RUN_OPS=(-C -c -r -p"${PROFILE}")
+RUN_OPS=(-C -c -r -p"${PROFILE}" -x"${GROUP}")
 
 # clean previous
 rm -rf log_* result_*
@@ -39,8 +42,11 @@ export BASE_DIR
 if ! "${SKIP_CONFIG}" ; then
 	export PERL5LIB="${BASE_DIR}/lib"
 	echo "add configuration for tests"
-	./bin/config_debug.pl on
-	ngcpcfg apply 'k-c-t on'
+	./bin/config_debug.pl -g"${GROUP}" on
+	(
+    cd /etc/ngcp-config || true
+    ngcpcfg apply "k-c-t ${GROUP} on"
+  )
 fi
 
 echo "$(date) - Starting $NUM tests"
@@ -67,6 +73,9 @@ set +o pipefail
 
 if ! "${SKIP_CONFIG}" ; then
 	echo "remove configuration for tests"
-	./bin/config_debug.pl off
-	ngcpcfg apply 'k-c-t off'
+	./bin/config_debug.pl -g"${GROUP}" off
+	(
+    cd /etc/ngcp-config || true
+    ngcpcfg apply "k-c-t ${GROUP} off"
+  )
 fi
