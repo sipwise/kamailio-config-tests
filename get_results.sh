25f2e51c05df1b8a7402172d34698de8e4afc448
Victor Seva <vseva@sipwise.com>
2020-07-08 16:24:31 +0200

    TT#81700 get_results.sh: add summary at the end of the report
    With all the retransmission checks, now is quite difficult to see
    what actually failed
    
    Change-Id: I194d8b5f7af772ea42a78fa99ec64a72b14434e3

--- get_results.sh
+++ get_results.sh
@@ -89,4 +89,9 @@ echo "${SCEN[@]}" |  tr ' ' '\n' \
  | parallel "${BIN_DIR}/check.sh ${GRAPH} -C -R ${OPTS} ${RETRANS} ${CDR} -d ${DOMAIN} -p ${PROFILE} -s ${GROUP}"
 status=$?
 echo "$(date) - All done[${status}]"
+tap_cmd=()
+for t in "${SCEN[@]}" ; do
+  tap_cmd+=( "result/${GROUP}/${t}/"*tap )
+done
+prove -f -Q "${tap_cmd[@]}"
 exit ${status}
