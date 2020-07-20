cdb1081378ff788a0ee5574d3116a6b0d8e1d2a6
Victor Seva <vseva@sipwise.com>
2020-07-20 17:48:21 +0200

    TT#85300 create_peers.pl: use try/catch when creating rules
    * search for the rules seems not to work fine
    * was unable to create more than one rule per group
    
    Change-Id: Ia0ef731968c7c1da6ab9874e2a5dbab604c42418

--- bin/create_peers.pl
+++ bin/create_peers.pl
@@ -21,6 +21,7 @@
 use strict;
 use warnings;
 
+use Try::Tiny;
 use English;
 use Getopt::Long;
 use Cwd 'abs_path';
@@ -120,13 +121,16 @@ sub manage_rules
 
   foreach my $rule (@{$data})
   {
-    $rule->{id} = $api->check_peeringrule_exists($rule);
-    if(defined $rule->{id}) {
-      print "rule: already there [$rule->{id}]\n";
-    } else {
+    try {
       $rule->{id} = $api->create_peeringrule($rule);
       print "rule: created [$rule->{id}]\n";
-    }
+    } catch {
+      if($opts->{verbose}) {
+        warn "rule: not able to create: $_";
+      } else {
+        warn "rule: not able to create";
+      }
+    };
   }
   return;
 }
@@ -137,13 +141,16 @@ sub manage_inbound_rules
 
   foreach my $rule (@{$data})
   {
-    $rule->{id} = $api->check_peeringinboundrule_exists($rule);
-    if(defined $rule->{id}) {
-      print "inboundrule: already there [$rule->{id}]\n";
-    } else {
+    try {
       $rule->{id} = $api->create_peeringinboundrule($rule);
       print "inboundrule: created [$rule->{id}]\n";
-    }
+    } else {
+      if($opts->{verbose}) {
+        warn "rule: not able to create: $_";
+      } else {
+        warn "rule: not able to create";
+      }
+    };
   }
   return;
 }
