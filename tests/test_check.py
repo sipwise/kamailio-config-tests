19c6581a5fabb65c2f986b101f1b6721b98cfe70
Victor Seva <vseva@sipwise.com>
2020-09-11 12:06:16 +0200

    TT#81700 bin/check.sh: add info of what sections failed
    * bin/check.py: return different values depending on what section
      check fails
      reformat using black tool
    
    Change-Id: Ie393766ba5df0951fb9f51c1c780cc5be8673fc7
    (cherry picked from commit 4c6d22737079ca31929264c23e6c6566ecdd69f3)

--- tests/test_check.py
+++ tests/test_check.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #
-# Copyright: 2013-2015 Sipwise Development Team <support@sipwise.com>
+# Copyright: 2013-2020 Sipwise Development Team <support@sipwise.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,109 +23,109 @@ import sys
 import junitxml
 import unittest
 import re
-lib_path = os.path.abspath('bin')
+
+lib_path = os.path.abspath("bin")
 sys.path.append(lib_path)
+from check import Section  # noqa
 from check import check_sip, check_sip_out  # noqa
 from check import XAvp, Test, check_flow, check_flow_vars  # noqa
 from check import load_json, load_yaml  # noqa
 
-not_ok = re.compile('^not ok.*', re.MULTILINE)
+not_ok = re.compile("^not ok.*", re.MULTILINE)
 
 
 class TestXAvp(unittest.TestCase):
-
     def setUp(self):
-        self.name = 'test'
+        self.name = "test"
         self.data = [
-            {'koko': [1]},
-            {'koko': [1, 2]},
-            {'lolo': [3], 'lola': [7]},
+            {"koko": [1]}, {"koko": [1, 2]}, {"lolo": [3], "lola": [7]}
         ]
-        self.xavp = XAvp('$xavp(%s)' % self.name, self.data)
+        self.xavp = XAvp("$xavp(%s)" % self.name, self.data)
 
     def test_init(self):
         self.assertEqual(self.name, self.xavp._name)
         self.assertCountEqual(self.data, self.xavp._data)
 
     def test_init_wrong_type(self):
-        self.assertRaises(Exception, self.xavp, '$var(whatever)', None)
+        self.assertRaises(Exception, self.xavp, "$var(whatever)", None)
 
     def test_parse_type(self):
         self.assertRaisesRegex(
-            Exception, 'no xavp', XAvp.parse, '$var(whatever)')
-        self.assertRaisesRegex(Exception, 'no xavp', XAvp.parse, '$fU')
+            Exception, "no xavp", XAvp.parse, "$var(whatever)"
+        )
+        self.assertRaisesRegex(Exception, "no xavp", XAvp.parse, "$fU")
 
     def test_get_wrong_name(self):
-        self.assertRaises(KeyError, self.xavp.get, '$xavp(otro)')
-        self.assertRaises(KeyError, self.xavp.get, '$xavp(otro[0])')
-        self.assertRaises(KeyError, self.xavp.get, '$xavp(otro=>whatever)')
-        self.assertRaises(KeyError, self.xavp.get, '$xavp(otro[0]=>whatever)')
+        self.assertRaises(KeyError, self.xavp.get, "$xavp(otro)")
+        self.assertRaises(KeyError, self.xavp.get, "$xavp(otro[0])")
+        self.assertRaises(KeyError, self.xavp.get, "$xavp(otro=>whatever)")
+        self.assertRaises(KeyError, self.xavp.get, "$xavp(otro[0]=>whatever)")
 
     def test_get_wrong_nindx(self):
-        self.assertRaises(IndexError, self.xavp.get, '$xavp(test[10]=>koko)')
+        self.assertRaises(IndexError, self.xavp.get, "$xavp(test[10]=>koko)")
         self.assertRaises(
-            IndexError, self.xavp.get, '$xavp(test[10]=>koko[1])')
+            IndexError, self.xavp.get, "$xavp(test[10]=>koko[1])"
+        )
 
     def test_get_wrong_kindx(self):
-        self.assertRaises(IndexError, self.xavp.get, '$xavp(test[0]=>koko[1])')
+        self.assertRaises(IndexError, self.xavp.get, "$xavp(test[0]=>koko[1])")
         self.assertRaises(
-            IndexError, self.xavp.get, '$xavp(test[1]=>koko[10])')
+            IndexError, self.xavp.get, "$xavp(test[1]=>koko[10])"
+        )
 
     def test_get_value(self):
-        self.assertEqual(self.xavp.get('$xavp(test=>koko)'), 1)
-        self.assertEqual(self.xavp.get('$xavp(test[1]=>koko[1])'), 2)
-        self.assertEqual(self.xavp.get('$xavp(test[2]=>lola[0])'), 7)
+        self.assertEqual(self.xavp.get("$xavp(test=>koko)"), 1)
+        self.assertEqual(self.xavp.get("$xavp(test[1]=>koko[1])"), 2)
+        self.assertEqual(self.xavp.get("$xavp(test[2]=>lola[0])"), 7)
 
     def test_get_value_all(self):
-        self.assertCountEqual(self.xavp.get('$xavp(test[1]=>koko[*])'), [1, 2])
+        self.assertCountEqual(self.xavp.get("$xavp(test[1]=>koko[*])"), [1, 2])
 
 
 class TestCheckFlowVars(unittest.TestCase):
-
     def setUp(self):
         self.ctest = Test()
         self.check_ok = [
-            {'R0': {'$xavp(v0)': [{'k0': [1],
-                                   'k1': ['a', 'b', 'fuckthisshit']},
-                                  {'k0': [1, 2], 'k1': ['a', ]},
-                                  ],
-                    'fU': 'testpep',
-                    }},
-            {'R1': {'$xavp(v0)': [{'k0': [1, 2]}]}},
+            {
+                "R0": {
+                    "$xavp(v0)": [
+                        {"k0": [1], "k1": ["a", "b", "fuckthisshit"]},
+                        {"k0": [1, 2], "k1": ["a"]},
+                    ],
+                    "fU": "testpep",
+                }
+            },
+            {"R1": {"$xavp(v0)": [{"k0": [1, 2]}]}},
         ]
-        self.check_ko = [
-            {'R0': {'$xavp(v0)': [{'k0': ['a', 'b']}]}},
-        ]
-        self.scen_ko = [
-            {'R0': {'$xavp(v0[0]=>k0[*])': ['a']}},
-        ]
-        self.scen_noxavp = [
-            {'R0': {'fU': 'testpep'}},
-            {'R1': {}},
-        ]
+        self.check_ko = [{"R0": {"$xavp(v0)": [{"k0": ["a", "b"]}]}}]
+        self.scen_ko = [{"R0": {"$xavp(v0[0]=>k0[*])": ["a"]}}]
+        self.scen_noxavp = [{"R0": {"fU": "testpep"}}, {"R1": {}}]
         self.scen = [
-            {'R0': {'$xavp(v0[0]=>k0[0])': 1,
-                    '$xavp(v0[0]=>k1[0])': 'a',
-                    '$xavp(v0[0]=>k1[2])': '^f',
-                    '$var(no)': 'None',
-                    '$xavp(nono=>koko)': 'None',
-                    '$xavp(v0=>k10)': 'None',
-                    '$xavp(v0[1]=>k0[1])': '\d+',
-                    '$xavp(v0[0]=>k1[*])': [1],
-                    '$xavp(v0[0]=>k1[*])': ['a', 'b', 'fuckthisshit']}
-             },
-            {'R1': {'$xavp(v0[1]=>k0[0])': 1}},
+            {
+                "R0": {
+                    "$xavp(v0[0]=>k0[0])": 1,
+                    "$xavp(v0[0]=>k1[0])": "a",
+                    "$xavp(v0[0]=>k1[2])": "^f",
+                    "$var(no)": "None",
+                    "$xavp(nono=>koko)": "None",
+                    "$xavp(v0=>k10)": "None",
+                    "$xavp(v0[1]=>k0[1])": r"\d+",
+                    "$xavp(v0[0]=>k1[*])": ["a", "b", "fuckthisshit"],
+                }
+            },
+            {"R1": {"$xavp(v0[1]=>k0[0])": 1}},
         ]
 
     def testXAvp(self):
-        data = self.check_ok[0]['R0']['$xavp(v0)']
-        xavp = XAvp('$xavp(v0)', data)
+        data = self.check_ok[0]["R0"]["$xavp(v0)"]
+        xavp = XAvp("$xavp(v0)", data)
 
-        self.assertEqual(xavp.get('$xavp(v0=>k0)'), 1)
+        self.assertEqual(xavp.get("$xavp(v0=>k0)"), 1)
         self.assertEqual(
-            xavp.get('$xavp(v0[0]=>k1[*])'), ['a', 'b', 'fuckthisshit'])
-        self.assertEqual(xavp.get('$xavp(v0[1]=>k0[1])'), 2)
-        self.assertEqual(xavp.get('$xavp(v0[1]=>k1[*])'), ['a'])
+            xavp.get("$xavp(v0[0]=>k1[*])"), ["a", "b", "fuckthisshit"]
+        )
+        self.assertEqual(xavp.get("$xavp(v0[1]=>k0[1])"), 2)
+        self.assertEqual(xavp.get("$xavp(v0[1]=>k1[*])"), ["a"])
 
     def testFlow_noxavp(self):
         check_flow(self.scen_noxavp, self.check_ok, self.ctest)
@@ -133,14 +133,14 @@ class TestCheckFlowVars(unittest.TestCase):
 
     def testFlowVars_noxavp(self):
         check_flow_vars(
-            'RO', self.scen_noxavp[0]['R0'],
-            self.check_ok[0]['R0'], self.ctest)
+            "RO", self.scen_noxavp[0]["R0"], self.check_ok[0]["R0"], self.ctest
+        )
         self.assertFalse(self.ctest.isError())
 
     def testFlowVars_xavp(self):
         check_flow_vars(
-            'RO', self.scen[0]['R0'],
-            self.check_ok[0]['R0'], self.ctest)
+            "RO", self.scen[0]["R0"], self.check_ok[0]["R0"], self.ctest
+        )
         self.assertFalse(self.ctest.isError())
 
     def testFlow_fail(self):
@@ -151,62 +151,57 @@ class TestCheckFlowVars(unittest.TestCase):
 
     def testFlowVars_fail(self):
         check_flow_vars(
-            'RO', self.scen_ko[0]['R0'],
-            self.check_ko[0]['R0'], self.ctest)
+            "RO", self.scen_ko[0]["R0"], self.check_ko[0]["R0"], self.ctest
+        )
         tap = str(self.ctest)
         self.assertTrue(self.ctest.isError(), tap)
         self.assertIsNotNone(not_ok.search(tap), tap)
 
 
 class TestCheckSipIn(unittest.TestCase):
-
     def setUp(self):
         self.ctest = Test()
-        self.msg = open('./tests/fixtures/sip_in.txt', 'r').read()
+        self.msg = open("./tests/fixtures/sip_in.txt", "r").read()
 
     def testSipIn(self):
-        sip_in = load_yaml('./tests/fixtures/test_sip_in.yml')
-        check_sip(sip_in, self.msg, self.ctest)
+        sip_in = load_yaml("./tests/fixtures/test_sip_in.yml")
+        check_sip(Section.SIP_IN, sip_in, self.msg, self.ctest)
         self.assertFalse(self.ctest.isError())
 
 
 class TestCheckSipOut(unittest.TestCase):
-
     def setUp(self):
         self.ctest = Test()
-        self.msg = load_yaml('./tests/fixtures/sip_out.yml')
+        self.msg = load_yaml("./tests/fixtures/sip_out.yml")
 
     def testSipOut(self):
-        sip_out = load_yaml('./tests/fixtures/test_sip_out.yml')
-        check_sip_out(sip_out, self.msg, self.ctest)
+        sip_out = load_yaml("./tests/fixtures/test_sip_out.yml")
+        check_sip_out(Section.SIP_OUT, sip_out, self.msg, self.ctest)
         self.assertFalse(self.ctest.isError())
 
 
 class TestJson(unittest.TestCase):
-
     def setUp(self):
         self.ctest = Test()
 
     def testFail(self):
-        check = load_json('./tests/fixtures/fail.json')
-        scen = load_yaml('./tests/fixtures/scen_fail.yml')
-        check_flow(scen['flow'], check['flow'], self.ctest)
+        check = load_json("./tests/fixtures/fail.json")
+        scen = load_yaml("./tests/fixtures/scen_fail.yml")
+        check_flow(scen["flow"], check["flow"], self.ctest)
         tap = str(self.ctest)
         self.assertTrue(self.ctest.isError(), tap)
         self.assertIsNotNone(not_ok.search(tap), tap)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     suite = unittest.TestSuite()
-    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestXAvp))
-    suite.addTest(
-        unittest.defaultTestLoader.loadTestsFromTestCase(TestCheckFlowVars))
-    suite.addTest(
-        unittest.defaultTestLoader.loadTestsFromTestCase(TestCheckSipIn))
-    suite.addTest(
-        unittest.defaultTestLoader.loadTestsFromTestCase(TestCheckSipOut))
-    suite.addTest(unittest.defaultTestLoader.loadTestsFromTestCase(TestJson))
+    load = unittest.defaultTestLoader.loadTestsFromTestCase
+    suite.addTest(load(TestXAvp))
+    suite.addTest(load(TestCheckFlowVars))
+    suite.addTest(load(TestCheckSipIn))
+    suite.addTest(load(TestCheckSipOut))
+    suite.addTest(load(TestJson))
     result = junitxml.JUnitXmlResult(sys.stdout)
     result.startTestRun()
     suite.run(result)
