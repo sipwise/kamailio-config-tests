c93bacb3ca034a10a09c2b6fdcbd33f386ecc149
Victor Seva <vseva@sipwise.com>
2020-12-04 12:37:31 +0100

    TT#81700 check: add check for rtpengine session per test
    * reduce rtpengine delete_delay to 2 secs
    
    Change-Id: Icd89d8800d14b306fcf9f7663ddefe9b80ac2978

--- bin/check.sh
+++ bin/check.sh
@@ -35,6 +35,10 @@ CDR=false
 ERR_FLAG=0
 RETRANS_SIZE=2
 
+rtpengine_ctl_ip=$(grep 'listen-cli' /etc/rtpengine/rtpengine.conf|\
+  awk '{print $3}')
+RTPENGINE_CTL="rtpengine-ctl -ip ${rtpengine_ctl_ip}"
+
 # $1 kamailio msg parsed to yml
 # $2 destination png filename
 graph() {
@@ -399,6 +403,7 @@ error_helper() {
   find "${SCEN_CHECK_DIR}/" -type f -name 'sipp_scenario*errors.log' \
     -exec mv {} "${LOG_DIR}" \;
   stop_capture
+  check_rtp
   exit "$2"
 }
 
@@ -427,6 +432,20 @@ stop_capture() {
       fi
     done
   fi
+}
+
+check_rtp() {
+  local uuid
+
+  uuid=$(awk '/^test_uuid:/ { print $2 }' "${SCEN_CHECK_DIR}/scenario.yml")
+  mapfile -t callids < <(${RTPENGINE_CTL} list sessions all|awk '{print $2}')
+  for callid in "${callids[@]}" ; do
+    if [[ "${callid}" =~ NGCP%${uuid}%/// ]]; then
+      echo "$(date) - Terminate RTP sessions for ${callid}"
+      ${RTPENGINE_CTL} terminate "${callid}"
+      ERR_FLAG=1
+    fi
+  done
 }
 
 # $1 port to check
@@ -919,10 +938,12 @@ if ! "$SKIP_RUNSIPP" ; then
         ((a++))
       done
     fi
-
     echo "$(date) - Done"
   fi
 
+  echo "$(date) - check RTP sessions, wait 5 secs first"
+  sleep 5
+  check_rtp
 fi
 
 
